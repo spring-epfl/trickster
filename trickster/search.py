70ef50b1014e028fe0f2d1e293b9a611b764cad8
Bogdan Kulynych <hello@bogdankulynych.me>
2018-06-05 23:50:34 +0200

    Small cleanup

--- trickster/search.py
+++ trickster/search.py
@@ -1,8 +1,6 @@
 import attr
-try:
-    from Queue import PriorityQueue
-except ImportError:
-    from queue import PriorityQueue
+
+from boltons.queueutils import PriorityQueue
 
 from .utils import fast_hash
 
@@ -20,56 +18,58 @@ def beam_a_star_search(
     Beam A* search with swappable parts.
 
     :param initial_node: Initial node.
-    :param expand_fn: Returns an iterable of tuples (cost, neighbour).
+    :param expand_fn: Returns an iterable of tuples (cost, candidate).
     :param heuristic_fn: Returns an estimate of the cost to the target
             node. By default, is a constant 0.
     :param iter_lim: Max number of iterations to try.
-    :param beam_size: Max number of expanded neighbours at each
+    :param beam_size: Max number of expanded candidates at each
             iteration (beam).
-    :param sort_neighbours: Whether to sort expanded neighbours at each
-            iteration.
-            If ``expand_fn`` is an infinite generator, you can set this
-            flag to ``True`` to expand ``beam_size`` neighbours each time.
     :param hash_fn: Hash function for nodes. By default set to
             :py:func:`utils.fash_hash`
     '''
 
     if heuristic_fn is None:
         heuristic_fn = lambda _: 0
+    if hash_fn is None:
+        hash_fn = lambda node: node
 
     iter_count = 0
     closed = set()
     pool = PriorityQueue()
 
-    # Calculate initial score
     score = heuristic_fn(initial_node)
+    cost_by_node = {}
 
-    pool.put((score, initial_node))
-    while not pool.empty() and (iter_lim is None or iter_count < iter_lim):
-        score, node = pool.get()
+    pool.add((0, initial_node), priority=-score)
+    while not len(pool) == 0 and (iter_lim is None or iter_count < iter_lim):
+        current_cost, node = pool.pop()
         if goal_predicate(node):
             return score, node
 
-        hashed = fast_hash(node)
+        hashed = hash_fn(node)
         if hashed in closed:
             continue
         closed.add(hashed)
 
-        # Generate neighbours, sort by cost if not sorted already.
-        neighbours = expand_fn(node)
-        if sort_neighbours:
-            neighbours = sorted(neighbours, key=lambda t: t[0])
-
-        for index, (cost, neighbour) in enumerate(neighbours):
-            # Neighbours that are too low in the ranking are cut off
+        # Generate candidates.
+        candidates = expand_fn(node)
+        for index, (transition_cost, candidate) in enumerate(candidates):
             if beam_size is not None and index >= beam_size:
                 break
-            if neighbour in closed:
+            if candidate in closed:
+                continue
+            candidate_hash = hash_fn(candidate)
+            candidate_cost = current_cost + transition_cost
+            if candidate_hash in cost_by_node and (
+                    candidate_cost > cost_by_node[candidate_hash]):
                 continue
 
             # Compute the total score.
-            score = cost + heuristic_fn(neighbour)
-            pool.put((score, neighbour))
+            score = candidate_cost + heuristic_fn(candidate)
+
+            # Add to the pool and the cost dictionary.
+            pool.add((candidate_cost, candidate), priority=-score)
+            cost_by_node[candidate_hash] = candidate_cost
 
         iter_count += 1
 
