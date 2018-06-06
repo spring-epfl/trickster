d51a2c9bbd4cb7033e5e2178e86b1bdd10b3e12e
Bogdan Kulynych <hello@bogdankulynych.me>
2018-06-06 16:20:42 +0200

    Make search format simpler; fix heuristic usage in tests

--- tests/test_search.py
+++ tests/test_search.py
@@ -33,18 +33,38 @@ def test_optimal_search_path(search_fn):
 
 @pytest.mark.parametrize('search_fn', HEURISTIC_SEARCH_FUNCS)
 @pytest.mark.parametrize('target_node', OPTIMAL_COSTS_FROM_ARAD.keys())
-def test_optimal_search_costs(search_fn, target_node):
+def test_optimal_search_target_node(search_fn, target_node):
     start_node = 'Arad'
     goal_fn = lambda x: x == target_node
 
-    goal, path_costs = search_fn(
+    # Do not use heuristic here, since it shows distance to Bucharest.
+    goal, cost = search_fn(
         start_node=start_node,
-        heuristic_fn=heuristic_fn,
         expand_fn=expand_fn,
         goal_fn=goal_fn,
         return_path=False
     )
 
+    assert goal == target_node
+    assert cost == OPTIMAL_COSTS_FROM_ARAD[goal]
+
+
+@pytest.mark.parametrize('search_fn', HEURISTIC_SEARCH_FUNCS)
+@pytest.mark.parametrize('target_node', OPTIMAL_COSTS_FROM_ARAD.keys())
+def test_optimal_search_costs(search_fn, target_node):
+    start_node = 'Arad'
+    goal_fn = lambda x: x == target_node
+
+    # Do not use heuristic here, since it shows distance to Bucharest.
+    goal, path_costs, optimal_path = search_fn(
+        start_node=start_node,
+        expand_fn=expand_fn,
+        goal_fn=goal_fn,
+        return_path=True
+    )
+
     for node, cost in path_costs.items():
         if node in optimal_path:
             assert cost == OPTIMAL_COSTS_FROM_ARAD[node]
+
+
