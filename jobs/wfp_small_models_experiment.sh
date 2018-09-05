984134678ceba6f7c9a51286234c01932a62c92b
Bogdan Kulynych <hello@bogdankulynych.me>
2018-09-05 18:15:55 +0200

    Add the experiment with multiple small WFP models

---
+++ jobs/wfp_small_models_experiment.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+NPROC=${NPROC:=4}
+NUM_MODELS=5
+TRAIN_DATASET_SIZE=112
+GENERATION_DATASET_SIZE=100
+GENERATION_MAX_TRACE_LEN=500
+ITER_LIM=100
+
+echo "Training small models..."
+seq $NUM_MODELS | \
+    parallel -j $NPROC \
+    scripts/wfp.py train \
+        --seed {} \
+        --shuffle \
+        --num_traces $TRAIN_DATASET_SIZE \
+        --model lr \
+        --features cumul \
+        --model_pickle "small_model_{}.pkl"
+
+echo "Generating adversarial examples..."
+seq $NUM_MODELS | \
+    parallel -j $NPROC \
+    scripts/wfp.py generate \
+        --log_file "log/wfo_small_models_{}.log" \
+        --model_pickle "small_model_{}.pkl" \
+        --iter_lim $ITER_LIM \
+        --features cumul \
+        --num_traces $GENERATION_DATASET_SIZE \
+        --max_trace_len $GENERATION_MAX_TRACE_LEN \
+        --output_pickle "results_small_model_{}.pkl"
