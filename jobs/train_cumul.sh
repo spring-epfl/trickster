88e6671c1a8f6a60631a8c652592ee1f3fa3fb27
Bogdan Kulynych <hello@bogdankulynych.me>
2018-11-20 20:34:48 +0100

    Add option to SVM RBF

--- jobs/train_cumul.sh
+++ jobs/train_cumul.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-MODEL_NAME="model_full_svmrbf_cumul"
+MODEL_TYPE=${MODEL_TYPE="lr"}
+MODEL_NAME="model_full_$MODEL_TYPE_cumul"
 
 echo "Training a model..."
 scripts/lr_cumul_wfp.py train \
-    --model svmrbf \
+    --model $MODEL_TYPE \
     --features cumul \
     --log_file "log/$MODEL_NAME.log" \
     --model_pickle "out/models/$MODEL_NAME.pkl"
