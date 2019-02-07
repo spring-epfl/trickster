d3f9eda78b2efbac1cb4392b91b3528510ebaf75
Bogdan Kulynych <hello@bogdankulynych.me>
2019-02-07 18:33:45 +0100

    Update bots high confidence experiments and their plotting

--- jobs/bots_generate_plots.sh
+++ jobs/bots_generate_plots.sh
@@ -3,6 +3,7 @@
 NPROC=${NPROC:=4}
 BANDS="['1k', '100k', '1M', '10M']"
 
+# Regular experiments.
 cd notebooks
 python -c "for band in $BANDS: print(band)" | \
     parallel -j $NPROC \
@@ -10,3 +11,10 @@ python -c "for band in $BANDS: print(band)" | \
             ../out/notebooks/bots_experiment_{}.ipynb \
             -p BAND {} \
             -p SAVE_PLOTS True
+
+# High confidence experiments.
+papermill bots_experiment.ipynb \
+    ../out/notebooks/bots_experiment_target_0.75.ipynb \
+    -p BAND '1k' \
+    -p SAVE_PLOTS True \
+    -p TARGET_CONFIDENCE 0.75 \
--- jobs/bots_high_confidence.sh
+++ jobs/bots_high_confidence.sh
@@ -1,28 +1,17 @@
 #!/bin/bash
 
-NPROC=${NPROC:=4}
-
 FOCUS_BINS="20"
 
-# Should be separated by commas. Choices of bands: "'1k', '100k', '1M', '10M'"
-BANDS="'1k'"
-
-# Should be separated by commas.
-CONFIDENCE_LEVELS="0.75, 0.95"
-
-# This one must not have commas. Sorry for the inconsistency.
-EPSILONS="0 1 5 10"
-
+# This one must not have commas.
+EPSILONS="0 1 2 3 5 10 100 500 1000"
 
 echo "Generating adversarial examples..."
-echo "import itertools
-for band, level in itertools.product([$BANDS], [$CONFIDENCE_LEVELS]): \
-print(band, level)" | python | \
-    parallel -j $NPROC --colsep ' ' \
-    scripts/bots.py \
-        $EPSILONS \
-        --log_file "log/bots_band_{1}_target_{2}.log" \
-        --bins $FOCUS_BINS \
-        --popularity_band {1} \
-        --confidence_level {2} \
-        --output_pickle "out/reports/bots_band_{1}_target_{2}.pkl"
+scripts/bots.py \
+    $EPSILONS \
+    --log_file "log/bots_band_1k_target_0.75.log" \
+    --bins $FOCUS_BINS \
+    --popularity_band 1k \
+    --confidence_level 0.75 \
+    --no_reduce_classifier \
+    --output_pickle "out/reports/bots_band_1k_target_0.75_more.pkl"
+
