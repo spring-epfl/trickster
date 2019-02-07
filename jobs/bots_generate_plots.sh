6514db91fd3f01e6d632c29d0a69948db2f45058
Bogdan Kulynych <hello@bogdankulynych.me>
2019-02-07 20:36:29 +0100

    Drop other bands from the plotting script

--- jobs/bots_generate_plots.sh
+++ jobs/bots_generate_plots.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
 NPROC=${NPROC:=4}
-BANDS="['1k', '100k', '1M', '10M']"
 
 # Regular experiments.
 cd notebooks
-python -c "for band in $BANDS: print(band)" | \
-    parallel -j $NPROC \
-        papermill bots_experiment.ipynb \
-            ../out/notebooks/bots_experiment_{}.ipynb \
-            -p BAND {} \
-            -p SAVE_PLOTS True
+papermill bots_experiment.ipynb \
+    ../out/notebooks/bots_experiment_band_1k.ipynb \
+    -p BAND '1k' \
+    -p SAVE_PLOTS True
 
 # High confidence experiments.
 papermill bots_experiment.ipynb \
-    ../out/notebooks/bots_experiment_target_0.75.ipynb \
+    ../out/notebooks/bots_experiment_band_1k_target_0.75.ipynb \
     -p BAND '1k' \
     -p SAVE_PLOTS True \
     -p TARGET_CONFIDENCE 0.75 \
