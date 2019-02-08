e7768efeee0131b4513afa29f1a2280609a27a3d
Bogdan Kulynych <hello@bogdankulynych.me>
2019-02-08 10:37:34 +0100

    Add report concatenation utility

---
+++ scripts/utils/report_cat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import sys
+import pickle
+
+
+if __name__ == '__main__':
+    if len(sys.argv) <= 1:
+        sys.exit(0)
+
+    joined_report = []
+    for report_path in sys.argv[1:]:
+        with open(report_path, 'rb') as f:
+            joined_report.extend(pickle.load(f))
+
+    sys.stdout.buffer.write(pickle.dumps(joined_report))
+
