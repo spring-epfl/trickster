3660908e9bc2395ba4b88bf9268c8e4e1245b10c
Bogdan Kulynych <hello@bogdankulynych.me>
2019-02-12 22:07:38 +0100

    Fix report_cat

--- scripts/utils/report_cat.py
+++ scripts/utils/report_cat.py
@@ -4,14 +4,13 @@ import sys
 import pickle
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) <= 1:
         sys.exit(0)
 
     joined_report = []
     for report_path in sys.argv[1:]:
-        with open(report_path, 'rb') as f:
+        with open(report_path, "rb") as f:
             joined_report.extend(pickle.load(f))
 
     sys.stdout.buffer.write(pickle.dumps(joined_report))
-
