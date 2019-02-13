dca17d2310298bddbd41a0759ff8db2cfe720808
Bogdan Kulynych <hello@bogdankulynych.me>
2019-02-13 22:30:49 +0100

    Add transferability experiments, restructure a bit

--- jobs/bots_generate_plots.sh
+++ jobs/bots_generate_plots.sh
@@ -5,7 +5,7 @@ NPROC=${NPROC:=4}
 # Regular experiments.
 cd notebooks
 papermill bots_experiment.ipynb \
-    ../out/notebooks/bots_experiment__band_1k.ipynb \
+    ../out/notebooks/bots_experiment__band_1k__target_50.ipynb \
     -p BAND '1k' \
     -p SAVE_PLOTS True
 
@@ -17,8 +17,21 @@ papermill bots_experiment.ipynb \
     -p TARGET_CONFIDENCE 75 \
 
 # Heuristics comparison experiments.
-papermill bots_heuristics_experiment.ipynb \
-    ../out/notebooks/bots_heuristics_experiment__band_1k.ipynb \
+papermill bots_heuristics.ipynb \
+    ../out/notebooks/bots_heuristics__band_1k.ipynb \
     -p BAND '1k' \
     -p SAVE_PLOTS True \
+    -p TARGET_CONFIDENCE 50 \
+
+# Transferability experiments.
+papermill bots_transferability.ipynb \
+    ../out/notebooks/bots_transferability__band_1k__target_50.ipynb \
+    -p BAND '1k' \
+    -p TARGET_CONFIDENCE 50
+
+papermill bots_transferability.ipynb \
+    ../out/notebooks/bots_transferability__band_1k__target_75.ipynb \
+    -p BAND '1k' \
     -p TARGET_CONFIDENCE 75 \
+    -p REPORT_PATH '../out/reports/bots__band_1k__target_75.pkl' \
+
