2b4e3853ce14884f79f21cd42a3d871fbd766fce
Bogdan Kulynych <hello@bogdankulynych.me>
2019-02-13 22:47:15 +0100

    Drop unnecessary papermill arg [CI skip]

--- jobs/bots_generate_plots.sh
+++ jobs/bots_generate_plots.sh
@@ -33,5 +33,4 @@ papermill bots_transferability.ipynb \
     ../out/notebooks/bots_transferability__band_1k__target_75.ipynb \
     -p BAND '1k' \
     -p TARGET_CONFIDENCE 75 \
-    -p REPORT_PATH '../out/reports/bots__band_1k__target_75.pkl' \
 
