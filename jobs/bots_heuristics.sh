62d3783637d7e112bfa8ddb0a178097dbd41e0a4
Bogdan Kulynych <hello@bogdankulynych.me>
2019-02-14 19:21:18 +0100

    Add hill climbing to the heuristics jobs

--- jobs/bots_heuristics.sh
+++ jobs/bots_heuristics.sh
@@ -1,46 +1,55 @@
 #!/bin/bash
 
 NPROC=${NPROC:=4}
-CONFIDENCE_LEVEL=50
 BINS=20
+BAND=1k
 
 EPSILONS="1"
 
 echo "Generating adversarial examples with L1 grid heuristic..."
 scripts/bots.py \
     $EPSILONS \
-    --log_file "log/bots__band_{}__target_50__h_grid.log" \
-    --popularity_band 1k \
+    --log_file "log/bots__band_${BAND}__target_50__h_grid.log" \
+    --popularity_band ${BAND} \
     --bins $BINS \
     --heuristic "dist_grid" \
-    --output_pickle "out/reports/bots__band_1k__target_50__h_grid.pkl"
+    --output_pickle "out/reports/bots__band_${BAND}__target_50__h_grid.pkl"
 
 echo "Generating adversarial examples with L2 heuristic..."
 scripts/bots.py \
     $EPSILONS \
-    --log_file "log/bots__band_1k__target_50__h_l2.log" \
-    --popularity_band 1k \
+    --log_file "log/bots__band_${BAND}__target_50__h_l2.log" \
+    --popularity_band ${BAND} \
     --bins $BINS \
     --p_norm 2 \
-    --output_pickle "out/reports/bots__band_1k__target_50__h_l2.pkl"
+    --output_pickle "out/reports/bots__band_${BAND}__target_50__h_l2.pkl"
 
 echo "Generating adversarial examples with Linf heuristic..."
 scripts/bots.py \
     $EPSILONS \
-    --log_file "log/bots__band_1k__target_50__h_linf.log" \
-    --popularity_band 1k \
+    --log_file "log/bots__band_${BAND}__target_50__h_linf.log" \
+    --popularity_band ${BAND} \
     --bins $BINS \
     --p_norm inf \
-    --output_pickle "out/reports/bots__band_1k__target_50__h_linf.pkl"
+    --output_pickle "out/reports/bots__band_${BAND}__target_50__h_linf.pkl"
+
+echo "Generating adversarial examples with hill climbing/L1 heuristic..."
+scripts/bots.py \
+    $EPSILONS \
+    --log_file "log/bots__band_${BAND}__target_50__hill_climbing.log" \
+    --popularity_band ${BAND} \
+    --bins $BINS \
+    --beam_size 1 \
+    --output_pickle "out/reports/bots__band_${BAND}__target_50__hill_climbing.pkl"
 
 echo "Generating adversarial examples with random heuristic..."
 seq 1 10 | parallel -j $NPROC \
     scripts/bots.py \
         $EPSILONS \
-        --log_file "log/bots__band_1k__target_50__h_random_seed_{}.log" \
+        --log_file "log/bots__band_${BAND}__target_50__h_random_seed_{}.log" \
         --bins $BINS \
-        --popularity_band 1k \
+        --popularity_band ${BAND} \
         --heuristic_seed {} \
         --heuristic random \
-        --output_pickle "out/reports/bots__band_1k__target_50__h_random_seed_{}.pkl"
+        --output_pickle "out/reports/bots__band_${BAND}__target_50__h_random_seed_{}.pkl"
 
