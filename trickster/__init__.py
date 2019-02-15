0eb211e00393f74b8f2e284bb55bbd10e254e62d
Bogdan Kulynych <hello@bogdankulynych.me>
2019-02-15 16:02:20 +0100

    Bump version

--- trickster/__init__.py
+++ trickster/__init__.py
@@ -1,7 +1,7 @@
 __title__ = "trickster"
 __author__ = "Bogdan Kulynych, Nikita Samarin"
 __email__ = "hello@bogdankulynych.me"
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 __copyright__ = "2018 by Bogdan Kulynych (EPFL SPRING Lab)"
 __description__ = "Library for attacking machine learning in discrete domains"
 __url__ = "https://github.com/spring-epfl/trickster"
