3888cec9694d76ddaa5f025443d2ad7b303bf4c7
Bogdan Kulynych <hello@bogdankulynych.me>
2019-02-26 18:20:12 +0100

    Add missing settings to generate plots job

--- jobs/bots_generate_plots.sh
+++ jobs/bots_generate_plots.sh
@@ -13,15 +13,16 @@ papermill bots_experiment.ipynb \
 papermill bots_experiment.ipynb \
     ../out/notebooks/bots_experiment__band_1k__target_75.ipynb \
     -p BAND '1k' \
-    -p SAVE_PLOTS True \
     -p TARGET_CONFIDENCE 75 \
+    -p SAVE_PLOTS True \
 
 # Heuristics comparison experiments.
-papermill bots_heuristics.ipynb \
-    ../out/notebooks/bots_heuristics__band_1k.ipynb \
+papermill bots_comparisons.ipynb \
+    ../out/notebooks/bots_comparisons__band_1k__target_50.ipynb \
     -p BAND '1k' \
-    -p SAVE_PLOTS True \
     -p TARGET_CONFIDENCE 50 \
+    -p HEURISTICS True \
+    -p SAVE_PLOTS True \
 
 # Transferability experiments.
 papermill bots_transferability.ipynb \
@@ -34,3 +35,18 @@ papermill bots_transferability.ipynb \
     -p BAND '1k' \
     -p TARGET_CONFIDENCE 75 \
 
+# Non-optimal experiments
+papermill bots_comparisons.ipynb \
+    ../out/notebooks/bots_comparisons__band_1k__target_75.ipynb \
+    -p BAND '1k' \
+    -p TARGET_CONFIDENCE 75 \
+    -p HEURISTICS False \
+    -p SAVE_PLOTS True \
+
+papermill bots_experiment.ipynb \
+    ../out/notebooks/bots_experiment__band_1k__target_50__model_svm.ipynb \
+    -p REPORT_PATH '../out/reports/bots__band_1k__target_50__model_svm.pkl' \
+    -p TARGET_CONFIDENCE 50 \
+    -p BAND '1k' \
+    -p MODEL 'svm' \
+    -p SAVE_PLOTS True
