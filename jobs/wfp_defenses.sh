c58d6cb8fff6803c78eef37729ca5949c8f03499
Bogdan Kulynych <hello@bogdankulynych.me>
2019-03-17 12:57:05 +0100

    Update the WFP defenses job

--- jobs/wfp_defenses.sh
+++ jobs/wfp_defenses.sh
@@ -5,20 +5,25 @@ NPROC=${NPROC:=4}
 TRACE_LEN=2000
 DATA_PATH=data/wfp/batch
 FILTERED_DATA_PATH=data/wfp/batch__tracelen_${TRACE_LEN}
-OUT_PREFIX=out/wfp_defenses
-DEFENSES="'buflo', 'cs_buflo', 'tamaraw', 'decoy'"
+OUT_PREFIX=out/wfp_defenses/trace_len_${TRACE_LEN}
 
-echo "Filtering traces..."
+# echo "Removing existing traces..."
+rm -rf ${OUT_PREFIX}/
+mkdir -p ${OUT_PREFIX}
+
+# echo "Filtering traces..."
 python scripts/wfp_defenses/filter_data.py ${TRACE_LEN}
 
-echo "Running defenses: $DEFENSES..."
-python -c "for defense in [$DEFENSES]: print(defense)" | \
+SIMPLE_DEFENSES="'buflo', 'cs_buflo', 'decoy'"
+echo "Running defenses: $SIMPLE_DEFENSES..."
+python -c "for defense in [$SIMPLE_DEFENSES]: print(defense)" | \
 parallel -j $NPROC \
     python scripts/wfp_defenses/{}.py --data_path ${FILTERED_DATA_PATH} --out_path ${OUT_PREFIX}/{}
 
 echo "Running WTF-Pad..."
-python scripts/wfp_defenses/wtfpad/src/main.py ${FILTERED_DATA_PATH} > /dev/null
 WTF_PAD_RESULTS=scripts/wfp_defenses/wtfpad/results
+python scripts/wfp_defenses/wtfpad/src/main.py ${FILTERED_DATA_PATH} --config normal_rcv > /dev/null
 WTF_PAD_TRACE_DIR=$(ls -t ${WTF_PAD_RESULTS} | head -1)
-mv ${WTF_PAD_RESULTS}/${WTF_PAD_TRACES}/* ${OUT_PREFIX}/wtfpad/
+mkdir -p ${OUT_PREFIX}/wtfpad
+mv ${WTF_PAD_RESULTS}/${WTF_PAD_TRACE_DIR}/* ${OUT_PREFIX}/wtfpad
 
